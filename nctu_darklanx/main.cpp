156dbe5c2a0ccca33c075d18cc6860274b44b648
Rabitomaster de Medici <littlecrazymouse.cs06@nctu.edu.tw>
2018-03-13 01:44:58 +0800

    Initial Commit

---
+++ nctu_darklanx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nctu_darklanx
+//
+//  Created by Darklanx on 2018/3/13.
+//  Copyright © 2018年 Darklanx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
